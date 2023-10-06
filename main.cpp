c8fc367350162b7be74656d0d1c0541780b4502f
AbdelrahmanAyman0011 <abdelrahmanayman0011@gmail.com>
2023-10-06 12:10:02 +0300

    new

--- main.cpp
+++ main.cpp
@@ -50,28 +50,36 @@ void file_i(string fname)
     freopen((fname + ".in").c_str(), "r", stdin);
 }
 
-void exec_time()
+void cv(vector<int> &arr, int n)
 {
-
-    using namespace std::chrono;
-    // Get starting timepoint
-    static auto start = high_resolution_clock::now();
-    static bool end = false;
-    // Get ending timepoint
-    if (end)
+    arr.resize(n);
+    for (int se = 0; se < n; se++)
     {
-        auto stop = high_resolution_clock::now();
-
-        // Get duration. Substart timepoints next
-        // get durarion. To cast it next proper unit
-        // use duration cast method
-        auto duration = duration_cast<milliseconds>(stop - start);
-
-        cout << "\nExec Time :"
-             << duration.count() << " milliseconds\n";
+        cin >> arr[se];
     }
-    else
-        end = true;
+}
+bool isPalindrome(int number)
+{
+    int originalNumber = number;
+    int reversedNumber = 0;
+
+    while (number > 0)
+    {
+        int remainder = number % 10;
+        reversedNumber = reversedNumber * 10 + remainder;
+        number /= 10;
+    }
+
+    return originalNumber == reversedNumber;
+}
+
+void rv(const vector<int> &arr, int n)
+{
+    for (int se = 0; se < n; se++)
+    {
+        cout << arr[se] << " ";
+    }
+    cout << endl;
 }
 
 int gcd(int a, int b)
@@ -102,32 +110,11 @@ int main()
         //    exec_time();
     }
     //-------------------------------solution--------------------------------
-    int c[3];
-    int m[3];
-
-    for (int i = 0; i < 3; i++)
-    {
-        cin >> c[i] >> m[i];
-    }
-
-    for (int i = 0; i < 100; i++)
-    {
-        int start = i % 3;
-        int next = (i + 1) % 3;
-        int pour = min(m[start], c[next] - m[next]);
-
-        m[start] -= pour;
-        m[next] += pour;
-    }
-
-    for (int i = 0; i < 3; i++)
-    {
-        cout << m[i] << endl;
-    }
+    int x;
+    cin >> x;
+    cout << x;
 
     //-----------------------------------------------------------------------
-    // if (testing)
-    //   exec_time();
 
     return 0;
 }
